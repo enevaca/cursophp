cfc9c6a6d49e61f866c0e46fe1b9d6af2622dde6
Noel Vaca (Ubuntu) <noel.vm.2501@gmail.com>
2018-01-29 15:45:23 -0400

    Arreglos

---
+++ variables/array.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 29/01/18
+ * Time: 15:37
+ */
+
+//$arrayVar = ['red', 'blue', 'black'];
+$arrayVar = [
+    'color1' => 'red',
+    'color2' => 'blue',
+    3 => 'black'
+];
+var_dump($arrayVar['color1']);
+
