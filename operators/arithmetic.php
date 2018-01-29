42afdcfde8ad616ee51d7b06f1bb68cb5fcaf947
Noel Vaca (Ubuntu) <noel.vm.2501@gmail.com>
2018-01-29 15:52:33 -0400

    Operadores Aritméticos

---
+++ operators/arithmetic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 29/01/18
+ * Time: 15:48
+ */
+
+$a = 10;
+$b = 3;
+
+//$c = $a / $b;
+//$c = (int)($a / $b);
+$c = $a % $b;
+var_dump($c);
