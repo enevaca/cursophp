86fa50eb3956772fe585e6198b9721d86c104980
Noel Vaca (Ubuntu) <noel.vm.2501@gmail.com>
2018-01-29 16:39:53 -0400

    Arrays

---
+++ operators/arrays.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 29/01/18
+ * Time: 16:30
+ */
+
+$array1 = [
+    1 => 'b',
+    2 => 'c',
+    0 => 'a'
+];
+
+//$array2 = [
+//    2 => 'd',
+//    4 => 'e'
+//];
+
+$array2 = ['a', 'b', 'c'];
+
+//$result = $array1 + $array2;
+//var_dump($result);
+//var_dump($array1 == $array2);
+var_dump($array1 === $array2);
