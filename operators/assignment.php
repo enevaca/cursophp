1e9f2900cf85c4b37b6f5956ed74f9d1c7e01e41
Noel Vaca (Ubuntu) <noel.vm.2501@gmail.com>
2018-01-29 16:05:38 -0400

    Operadores de Asignación

---
+++ operators/assignment.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 29/01/18
+ * Time: 15:59
+ */
+
+$a = 'Hello ';
+
+//$a = $a + 5;
+//$a += 5;
+//$a *= 5;
+
+$a .= 'World';
+var_dump($a);
