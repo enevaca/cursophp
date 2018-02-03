a5156b30a3379bfbf992eacb9970c59535033fef
enevaca <noel.vm.2501@gmail.com>
2018-02-03 17:01:19 -0400

    Cargas de Archivos Externos

---
+++ functions/external/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 3/02/18
+ * Time: 16:52
+ */
+
+echo '<p>Text from functions.php</p>';
+
+function sum($a, $b) {
+    $result = $a + $b;
+    echo '<p>Result: ' . $result . ' </p>';
+}
---
+++ functions/external/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 3/02/18
+ * Time: 16:52
+ */
+
+//include 'functions.php'; // no se detiene en error
+//include_once 'functions.php'; // no se detiene en error
+require 'functions.php'; // si se detiene en error
+
+echo '<p>Text from index.php</p>';
+
+sum(10, 20);
