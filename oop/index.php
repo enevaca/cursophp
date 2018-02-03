3f932e15dc38f82dff2a82607eec784bad3524a4
enevaca <noel.vm.2501@gmail.com>
2018-02-03 18:20:52 -0400

    Constructor y Destructor

--- oop/index.php
+++ oop/index.php
@@ -11,32 +11,47 @@ class Car
     //private $owner;
     private $owner;
 
-    public function move() {
+    public function __construct($owner)
+    {
+        $this->owner = $owner;
+        echo 'Constructor<br>';
+
+    }
+
+    public function __destruct()
+    {
+        echo 'Destruct<br>';
+    }
+
+    public function move()
+    {
         echo 'moving<br>';
     }
 
-    public function getOwner() {
+    public function getOwner()
+    {
         return $this->owner;
     }
 
-    public function setOwnser($owner) {
+    public function setOwnser($owner)
+    {
         $this->owner = $owner;
     }
 }
 
 echo 'Class Car<br>';
 
-$car = new Car();
+$car = new Car('Esnor');
+$car2 = new Car('Roxana');
 $car->move();
 
 //$car->owner = 'Noel';
 //echo 'Owner: ' . $car->owner;
 
-$car->setOwnser('Noel');
-
-$car2 = new Car();
-$car2->setOwnser('Roxana');
+//$car->setOwnser('Noel');
+//$car2->setOwnser('Roxana');
 
 echo 'Owner Car: ' . $car->getOwner();
 echo '<br>';
 echo 'Owner Car2: ' . $car2->getOwner();
+echo '<br>';
