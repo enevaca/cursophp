5efbdf9138243cca39fd7dd687afba13f9188fe3
enevaca <noel.vm.2501@gmail.com>
2018-02-04 17:11:35 -0400

    Conexión desde PHP a una base de datos SQL

---
+++ databases/index.php
@@ -0,0 +1,38 @@
+<?php
+$dbHost = 'localhost';
+$dbName = 'cursophp';
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Databases with Platzi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Databases</h1>
+        <ul>
+            <li>
+                <a href="">List Users</a>
+            </li>
+            <li>
+                <a href="">Add User</a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
