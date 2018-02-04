2077dae331724b52f527475922a88a2ae5683b5f
enevaca <noel.vm.2501@gmail.com>
2018-02-04 16:21:43 -0400

    Excepciones

---
+++ oop/exceptions/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roxana
+ * Date: 04/02/2018
+ * Time: 9:59
+ */
+
+include 'vehicles/Car.php';
+include 'vehicles/Truck.php';
+include 'vehicles/ToyCar.php';
+
+//use Vehicles\Car;
+//use Vehicles\Truck;
+use Vehicles4\{
+    Car, Truck, ToyCar
+};  //Uso a partir de PHP 7
+
+try {
+    echo 'Class ToyCar<br>';
+    $toyCar = new ToyCar('Esnor');
+    $toyCar->move();
+} catch (Exception $e) {
+    echo 'This is a toy<br>';
+    //log
+} finally {
+    echo 'finally<br><br>';
+}
+
+echo 'Class Car<br>';
+$car = new Car('Alex');
+$car->move();
+echo 'Owner car: ' . $car->getOwner() . '<br>';
+
+echo '<br>Class Truck<br>';
+$truck = new Truck('Maximo', 'Pickup');
+$truck->move();
+echo 'Owner truck: ' . $truck->getOwner() . '<br>';
+
+//propiedades estaticas
+echo '<br>Class Truck 1<br>';
+$truck1 = new Truck('Maximo', 'Pickup');
+$truck1->move();
+
+echo '<br>Class Truck2<br>';
+$truck2 = new Truck('Maximo', 'Pickup');
+$truck2->move();
+
+//echo '<br>Total Trucks: ' . Truck::$count . '<br>';   //propiedad publica
+echo '<br>Total Trucks: ' . Truck::getTotal() . '<br>';   //propiedad privada, acceso a metodo publico
+
+//interfaces
+$ser = serialize($car);
+$newCar = unserialize($ser);
+
+echo 'NewCar Owner: ' . $newCar->getOwner() . '<br>';
+//
---
+++ oop/exceptions/vehicles/ToyCar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noel
+ * Date: 4/02/18
+ * Time: 16:06
+ */
+
+namespace Vehicles4;
+
+require_once 'VehicleBase.php';
+
+class ToyCar extends VehicleBase {
+    public function move()
+    {
+        //abstract
+        echo $this->startEngine();
+        //
+        echo '<br>';
+        echo 'Car2: moving<br>';
+    }
+
+    public function startEngine()
+    {
+        throw new \Exception('Engine not found');
+    }
+}
